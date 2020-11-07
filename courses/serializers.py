e528b578bef1d9232ddaf6ada50c0d279c6be817
Zahra Salehi <zahrasalehii19988@gmail.com>
2020-11-07 14:51:23 +0330

    initial commit

---
+++ courses/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from . import models
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+
+        fields = (
+            'id',
+            'course',
+            'name',
+            'email',
+            'comment',
+            'rating',
+            'created_at',
+
+        )
+
+        model = models.Review
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    reviews = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='apiv2:review-detail'
+    )
+
+    class Meta:
+        fields = (
+            'id',
+            'title',
+            'url',
+            'reviews'
+        )
+        model = models.Course
